b0c1757339fb8fb0ebeab47866d67d496734b0f7
Quipex <rudas72@gmail.com>
2018-11-07 02:33:02 +0200

    simple and rough realization of the interface

---
+++ src/main/java/com/nc/airport/backend/model/EntityFactoryImpl.java
@@ -0,0 +1,13 @@
+package com.nc.airport.backend.model;
+
+public class EntityFactoryImpl implements EntityFactory {
+    @Override
+    public <T extends Entity> T getNewEntity(Class<T> clazz) {
+        try {
+            return clazz.newInstance();
+        } catch (InstantiationException | IllegalAccessException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+}
