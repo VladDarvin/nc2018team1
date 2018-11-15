a90b03f2fac3512401b26bb7235a40cf327b0c9f
Quipex <rudas72@gmail.com>
2018-11-15 14:28:50 +0200

    Removed wrong implementation

--- src/main/java/com/nc/airport/backend/eav/MutableFactoryImpl.java
+++
@@ -1,8 +0,0 @@
-package com.nc.airport.backend.eav;
-
-public class MutableFactoryImpl implements MutableFactory {
-    @Override
-    public Mutable getNewMutable() {
-        return new Mutable();
-    }
-}
