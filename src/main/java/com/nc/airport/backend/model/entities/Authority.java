b1334126dde8bf8db6a0f437e0fade79e3344024
Denis Zubov <json96ua@gmail.com>
2018-11-21 23:44:12 +0200

    Add Authority entity for authentication

---
+++ src/main/java/com/nc/airport/backend/model/entities/Authority.java
@@ -0,0 +1,45 @@
+package com.nc.airport.backend.model.entities;
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+public class Authority {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private Long id;
+
+    private String name;
+
+    @ManyToMany(mappedBy = "authorities", fetch = FetchType.LAZY)
+    @JsonBackReference
+    private List<User> users;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<User> getUsers() {
+        return users;
+    }
+
+    public void setUsers(List<User> users) {
+        this.users = users;
+    }
+}
