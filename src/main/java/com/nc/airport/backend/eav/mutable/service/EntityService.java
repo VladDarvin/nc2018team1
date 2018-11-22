6145419ee9d589c5dfd18cbd44f3437d8d7ef87e
Quipex <rudas72@gmail.com>
2018-11-23 00:27:54 +0200

    Reworked mutable-service class placement

--- src/main/java/com/nc/airport/backend/eav/mutable/service/EntityService.java
+++ src/main/java/com/nc/airport/backend/eav/mutable/service/EntityService.java
@@ -5,11 +5,10 @@ import com.nc.airport.backend.model.BaseEntity;
 
 /**
  * A service that can convert mutables to childs of BaseEntity and backwards
- * @param <T>
  */
-public interface EntityService<T extends BaseEntity> {
+public interface EntityService {
 
-    T convertMutableToEntity(Mutable mutable, Class<T> clazz);
+    <T extends BaseEntity> T convertMutableToEntity(Mutable mutable, Class<T> clazz);
 
-    Mutable convertEntityToMutable(T entity);
+    Mutable convertEntityToMutable(BaseEntity entity);
 }
--- src/main/java/com/nc/airport/backend/model/EntityFactory.java
+++
@@ -1,6 +0,0 @@
-package com.nc.airport.backend.model;
-
-public interface EntityFactory {
-
-    <T extends BaseEntity> T getNewEntity(Class<T> clazz);
-}
