2cc64e8fb7d907a0e29b61b72b742784fd5c3109
Quipex <rudas72@gmail.com>
2018-12-09 18:18:16 +0200

    added tostring, hashcode and equals for testing

--- src/main/java/com/nc/airport/backend/model/BaseEntity.java
+++ src/main/java/com/nc/airport/backend/model/BaseEntity.java
@@ -2,17 +2,35 @@ package com.nc.airport.backend.model;
 
 import lombok.Getter;
 import lombok.Setter;
+import lombok.ToString;
 
 import java.math.BigInteger;
+import java.util.Objects;
 
 /**
  * Base class for POJOs that are intended to be converted into mutable
  */
-@Getter
 @Setter
+@Getter
+@ToString
 public abstract class BaseEntity {
     private BigInteger objectId;
     private BigInteger parentId;
     private String objectName;
     private String objectDescription;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        BaseEntity that = (BaseEntity) o;
+        return Objects.equals(objectId, that.objectId) &&
+                Objects.equals(parentId, that.parentId) &&
+                Objects.equals(objectName, that.objectName);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(objectId, parentId);
+    }
 }
