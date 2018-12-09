088d4edb10116c737242c6d1d4b714b0f15f86ac
Quipex <rudas72@gmail.com>
2018-12-09 02:16:22 +0200

    formatted and reworked query

--- src/main/java/com/nc/airport/backend/repository/UsersRepository.java
+++ src/main/java/com/nc/airport/backend/repository/UsersRepository.java
@@ -11,19 +11,27 @@ import org.springframework.data.repository.PagingAndSortingRepository;
 import java.util.List;
 
 public interface UsersRepository extends PagingAndSortingRepository<User, Long>, JpaSpecificationExecutor<User> {
-    //    @Query(value = "select * from \"USER\" inner join AUTHORITY on \"USER\".AUTHORITY_ID = AUTHORITY.ID", nativeQuery = true)
-    @Query(value = "select * from USERS inner join AUTHORITY on USERS.AUTHORITY_ID = AUTHORITY.ID", nativeQuery = true)
+
+    @Query(value = "SELECT * FROM USERS INNER JOIN AUTHORITY ON USERS.AUTHORITY_ID = AUTHORITY.ID ORDER BY USERS.ID", nativeQuery = true)
     List<User> getAll();
 
     Page<User> findAll(Specification<User> spec, Pageable pageable);
 
     User findUserByEmailAndPassword(String email, String password);
+
     List<User> findAllByOrderByFirstnameAsc();
+
     List<User> findAllByOrderByFirstnameDesc();
+
     List<User> findAllByOrderByLastnameAsc();
+
     List<User> findAllByOrderByLastnameDesc();
+
     List<User> findAllByOrderByEmailAsc();
+
     List<User> findAllByOrderByEmailDesc();
+
     List<User> findAllByOrderByPhonenumberAsc();
+
     List<User> findAllByOrderByPhonenumberDesc();
 }
