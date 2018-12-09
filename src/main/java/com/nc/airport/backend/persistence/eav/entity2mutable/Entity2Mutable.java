26e936e1e9ae861d7cf320f750c96ccb0e66b226
Quipex <rudas72@gmail.com>
2018-12-09 22:17:25 +0200

    added javadoc

--- src/main/java/com/nc/airport/backend/persistence/eav/entity2mutable/Entity2Mutable.java
+++ src/main/java/com/nc/airport/backend/persistence/eav/entity2mutable/Entity2Mutable.java
@@ -8,7 +8,21 @@ import com.nc.airport.backend.persistence.eav.Mutable;
  */
 public interface Entity2Mutable {
 
+    /**
+     * Converts mutable into entity of supplied type
+     *
+     * @param mutable initial mutable
+     * @param clazz   supplied type
+     * @param <T>     extends BaseEntity
+     * @return entity of supplied type
+     */
     <T extends BaseEntity> T convertMutableToEntity(Mutable mutable, Class<T> clazz);
 
+    /**
+     * Converts entity into mutable
+     *
+     * @param entity initial entity
+     * @return mutable that corresponds to supplied entity
+     */
     Mutable convertEntityToMutable(BaseEntity entity);
 }
