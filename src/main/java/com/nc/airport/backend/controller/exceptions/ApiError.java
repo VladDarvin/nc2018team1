2527906dbb4c60524a9eb1782f7354818aa32d45
do-While-on-GitHub <atterdag@zoho.eu>
2018-12-17 18:46:17 +0200

    Common REST API errors handling

--- src/main/java/com/nc/airport/backend/controller/exceptions/ApiError.java
+++ src/main/java/com/nc/airport/backend/controller/exceptions/ApiError.java
@@ -10,6 +10,22 @@ import java.time.LocalDateTime;
 @Setter
 @Getter
 public class ApiError {
+    /**
+     *@param status holds the operation call status.
+     *              It will be anything from 4xx to signalize client errors or 5xx to mean server errors.
+     *              A common scenario is a http code 400 that means a BAD_REQUEST,
+     *              when the client, for example, sends an improperly formatted field, like an invalid email address.
+     *
+     * @param timestamp holds the date-time instance of when the error happened.
+     *
+     * @param message holds a user-friendly message about the error.
+     *
+     * @param debugMessage holds a system message describing the error in more detail.
+     *
+     * @param subErrors holds an array of sub-errors that happened.
+     *                  This is used for representing multiple errors in a single call.
+     *                  An example would be validation errors in which multiple fields have failed the validation.
+     */
     private HttpStatus status;
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy hh:mm:ss")
     private LocalDateTime timestamp;
@@ -28,7 +44,7 @@ public class ApiError {
     ApiError(HttpStatus status, Throwable ex) {
         this();
         this.status = status;
-        this.message = "Unexpected error";
+        this.message = "Unexpected or unknown error";
         this.debugMessage = ex.getLocalizedMessage();
     }
 
--- src/main/java/com/nc/airport/backend/controller/exceptions/RestExceptionHandler.java
+++ src/main/java/com/nc/airport/backend/controller/exceptions/RestExceptionHandler.java
@@ -1,22 +1,90 @@
 package com.nc.airport.backend.controller.exceptions;
 
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.web.HttpMediaTypeNotSupportedException;
+import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import javax.persistence.EntityExistsException;
+import javax.persistence.EntityNotFoundException;
 
 @ControllerAdvice
 public class RestExceptionHandler extends ResponseEntityExceptionHandler {
 
+    /**
+     * Handle all exceptions that don’t have specific exception handler
+     */
+    @ExceptionHandler(Exception.class)
+    protected ResponseEntity<Object> defaultExceptionHandler(Exception ex) {
+        ApiError apiError = new ApiError(HttpStatus.BAD_REQUEST, ex);
+        return buildResponseEntity(apiError);
+    }
+
+    /**
+     * Handle exception when method arguments are not the expected type
+     */
+    @ExceptionHandler(MethodArgumentTypeMismatchException.class)
+    protected ResponseEntity<Object> handleMethodArgumentTypeMismatch(
+            MethodArgumentTypeMismatchException ex, WebRequest request) {
+        ApiError apiError = new ApiError(HttpStatus.BAD_REQUEST);
+        apiError.setMessage(String.format(
+                "The parameter '%s' of value '%s' could not be converted to type '%s'", ex.getName(), ex.getValue(), ex.getRequiredType().getSimpleName()));
+        apiError.setDebugMessage(ex.getMessage());
+        return buildResponseEntity(apiError);
+    }
+
+    /**
+     *  Handle exception when API is not able to read the HTTP message
+     */
+    protected ResponseEntity<Object> handleHttpMessageNotReadable(HttpMessageNotReadableException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
+        String error = "Malformed JSON request";
+        return buildResponseEntity(new ApiError(HttpStatus.BAD_REQUEST, error, ex));
+    }
+
+    /**
+     * Handle exception when JSON is invalid as well.
+     */
+    protected ResponseEntity<Object> handleHttpMediaTypeNotSupported(
+            HttpMediaTypeNotSupportedException ex,
+            HttpHeaders headers,
+            HttpStatus status,
+            WebRequest request) {
+        StringBuilder builder = new StringBuilder();
+        builder.append(ex.getContentType());
+        builder.append(" media type is not supported. Supported media types are ");
+        ex.getSupportedMediaTypes().forEach(t -> builder.append(t).append(", "));
+        return buildResponseEntity(new ApiError(HttpStatus.UNSUPPORTED_MEDIA_TYPE, builder.substring(0, builder.length() - 2), ex));
+    }
+
+    /**
+     * Handle exception when entity already exist.
+     */
     @ExceptionHandler(EntityExistsException.class)
     protected ResponseEntity<Object> handleEntityAlreadyExist(EntityExistsException ex) {
         ApiError apiError = new ApiError(HttpStatus.CONFLICT);
         apiError.setMessage(ex.getMessage());
         return buildResponseEntity(apiError);
     }
+
+    /**
+     * Handle exception when entity doesn't exist.
+     */
+    @ExceptionHandler(EntityNotFoundException.class)
+    protected ResponseEntity<Object> handleEntityNotFound(
+            EntityNotFoundException ex) {
+        ApiError apiError = new ApiError(HttpStatus.NOT_FOUND);
+        apiError.setMessage(ex.getMessage());
+        return buildResponseEntity(apiError);
+    }
+
+
 
     private ResponseEntity<Object> buildResponseEntity(ApiError apiError) {
         return new ResponseEntity<>(apiError, apiError.getStatus());
