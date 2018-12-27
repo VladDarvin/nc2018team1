f52ebe1c630d6966ef3e4c9874fc71c98058228c
json96ua <42866143+json96ua@users.noreply.github.com>
2018-12-27 17:07:26 +0200

    Add sorting and filtering dto

---
+++ src/main/java/com/nc/airport/backend/model/dto/SortingFilteringWrapper.java
@@ -0,0 +1,19 @@
+package com.nc.airport.backend.model.dto;
+
+import com.nc.airport.backend.persistence.eav.mutable2query.filtering2sorting.sorting.SortEntity;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import java.math.BigInteger;
+import java.util.List;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class SortingFilteringWrapper {
+    String searchString;
+    List<SortEntity> sortList;
+
+}
