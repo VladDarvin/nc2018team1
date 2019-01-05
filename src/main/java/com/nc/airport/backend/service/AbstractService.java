ba5aa8b4693d22e0c4f13d50e7ddaa5bd61374ae
json96ua <42866143+json96ua@users.noreply.github.com>
2019-01-05 15:56:19 +0200

    Fix a bag in AbstractService

--- src/main/java/com/nc/airport/backend/service/AbstractService.java
+++ src/main/java/com/nc/airport/backend/service/AbstractService.java
@@ -1,16 +1,16 @@
 package com.nc.airport.backend.service;
 
-import com.nc.airport.backend.model.entities.model.flight.Country;
+import com.nc.airport.backend.model.BaseEntity;
 import com.nc.airport.backend.persistence.eav.entity2mutable.util.ReflectionHelper;
 import com.nc.airport.backend.persistence.eav.mutable2query.filtering2sorting.filtering.FilterEntity;
 
 import java.math.BigInteger;
 import java.util.*;
 
-public abstract class AbstractService {
-    protected List<FilterEntity> makeFilterList(String search) {
+public abstract class AbstractService<T extends BaseEntity> {
+    protected List<FilterEntity> makeFilterList(String search, Class<T> entityClass) {
         String searchString = "%" + search + "%";
-        List<BigInteger> attributeIds = ReflectionHelper.getAttributeIds(Country.class);
+        List<BigInteger> attributeIds = ReflectionHelper.getAttributeIds(entityClass);
         Map<BigInteger, Set<Object>> filtering = new HashMap<>();
         for (BigInteger id :
                 attributeIds) {
