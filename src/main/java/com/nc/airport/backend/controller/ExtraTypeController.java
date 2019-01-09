d638c5ee9f9c9cd2a15096377a1c91668d9e01d0
do-While-on-GitHub <atterdag@zoho.eu>
2019-01-09 07:04:57 +0200

    Implement controller and service for ExtraTypes.

---
+++ src/main/java/com/nc/airport/backend/controller/ExtraTypeController.java
@@ -0,0 +1,57 @@
+package com.nc.airport.backend.controller;
+
+import com.nc.airport.backend.model.dto.ResponseFilteringWrapper;
+import com.nc.airport.backend.model.dto.SortingFilteringWrapper;
+import com.nc.airport.backend.model.entities.model.airplane.ExtraType;
+import com.nc.airport.backend.service.ExtraTypeService;
+import org.springframework.web.bind.annotation.*;
+
+import java.math.BigInteger;
+import java.util.List;
+
+@RestController
+@CrossOrigin
+@RequestMapping("extra-type")
+public class ExtraTypeController {
+    private ExtraTypeService extraTypeService;
+
+    public ExtraTypeController(ExtraTypeService extraTypeService) {
+        this.extraTypeService = extraTypeService;
+    }
+
+    @GetMapping("/page={page}")
+    public List<ExtraType> getTenExtraTypes(@PathVariable(name = "page") int page) {
+        return extraTypeService.getTenEntities(page);
+    }
+
+    @PostMapping
+    public ExtraType addNewExtraType(@RequestBody ExtraType airplane) {
+        return extraTypeService.saveEntity(airplane);
+    }
+
+    @PutMapping
+    public ExtraType editExtraType(@RequestBody ExtraType airplane) {
+        return extraTypeService.saveEntity(airplane);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteExtraType(@PathVariable(name = "id") BigInteger id) {
+        extraTypeService.deleteEntity(id);
+    }
+
+    @GetMapping("/count")
+    public BigInteger getCountOfExtraTypes() {
+        return extraTypeService.getEntitiesAmount();
+    }
+
+    @GetMapping("/count/search={searchString}")
+    public BigInteger getCountOfExtraTypesByFilter(@PathVariable(name = "searchString") String searchString) {
+        return extraTypeService.getAmountOfFilteredEntities(searchString);
+    }
+
+    @PostMapping("/search/page={page}")
+    public ResponseFilteringWrapper searchExtraTypes(@PathVariable(name = "page") int page,
+                                                    @RequestBody SortingFilteringWrapper wrapper) {
+        return extraTypeService.filterAndSortEntities(page, wrapper.getSearchString(), wrapper.getSortList());
+    }
+}
---
+++ src/main/java/com/nc/airport/backend/service/ExtraTypeService.java
@@ -0,0 +1,13 @@
+package com.nc.airport.backend.service;
+
+import com.nc.airport.backend.model.entities.model.airplane.ExtraType;
+import com.nc.airport.backend.persistence.eav.repository.EavCrudRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ExtraTypeService extends AbstractService<ExtraType>{
+
+    public ExtraTypeService(EavCrudRepository<ExtraType> repository) {
+        super(ExtraType.class, repository);
+    }
+}
