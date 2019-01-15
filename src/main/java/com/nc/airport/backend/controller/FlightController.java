12d5f6aa2a63736a32b74f8a00ed09c55f30f12c
json96ua <42866143+json96ua@users.noreply.github.com>
2019-01-15 16:04:26 +0200

    Add FlightController

---
+++ src/main/java/com/nc/airport/backend/controller/FlightController.java
@@ -0,0 +1,29 @@
+package com.nc.airport.backend.controller;
+
+import com.nc.airport.backend.model.dto.FlightDTO;
+import com.nc.airport.backend.model.entities.model.users.User;
+import com.nc.airport.backend.service.FlightService;
+import com.nc.airport.backend.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
+
+@RestController
+@CrossOrigin(origins = "http://localhost:4200")
+public class FlightController {
+    private FlightService flightService;
+    private UserService userService;
+
+    @Autowired
+    public FlightController(FlightService flightService,
+                               UserService userService) {
+        this.flightService = flightService;
+        this.userService = userService;
+    }
+
+    @RequestMapping(value = "/flights/userLogin={userLogin}/page={page}", method = RequestMethod.GET)
+    public List<FlightDTO> getAllFlights(@PathVariable String userLogin, @PathVariable int page) {
+        User parentUser = userService.findByLogin(userLogin);
+        return flightService.findAllFlightsByUserId(parentUser.getObjectId(), page);
+    }
+}
