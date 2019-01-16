2e9d5aa5ba43d3b2fe4a1b1994406addfdf5dfaa
json96ua <42866143+json96ua@users.noreply.github.com>
2019-01-16 12:58:46 +0200

    Fix date format

--- src/main/java/com/nc/airport/backend/model/entities/model/ticketinfo/Passport.java
+++ src/main/java/com/nc/airport/backend/model/entities/model/ticketinfo/Passport.java
@@ -1,5 +1,6 @@
 package com.nc.airport.backend.model.entities.model.ticketinfo;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.nc.airport.backend.model.BaseEntity;
 import com.nc.airport.backend.persistence.eav.annotations.ObjectType;
 import com.nc.airport.backend.persistence.eav.annotations.attribute.value.DateField;
@@ -7,7 +8,6 @@ import com.nc.airport.backend.persistence.eav.annotations.attribute.value.ValueF
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
-
 import java.time.LocalDateTime;
 
 @ObjectType(ID = "14")
@@ -23,5 +23,6 @@ public class Passport extends BaseEntity {
     private String country;
 
     @DateField(ID = "43")
+    @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss")
     private LocalDateTime birthDate;
 }
