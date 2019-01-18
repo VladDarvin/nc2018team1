8ca96fb6c7b8749ddbffebf85e6cc7e24278da97
json96ua <42866143+json96ua@users.noreply.github.com>
2019-01-18 15:00:43 +0200

    Fixes

--- src/main/java/com/nc/airport/backend/model/entities/model/ticketinfo/Ticket.java
+++ src/main/java/com/nc/airport/backend/model/entities/model/ticketinfo/Ticket.java
@@ -25,8 +25,8 @@ public class Ticket extends BaseEntity {
     @ReferenceField(ID = "32")
     private BigInteger passengerId;
 
-    @ListField(ID = "33")
-    private TicketStatus ticketStatus;
+    @ReferenceField(ID = "33")
+    private BigInteger ticketStatus;
 
 //   ?
 
