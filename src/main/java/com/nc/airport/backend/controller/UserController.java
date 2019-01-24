9998b3c650e136642962381607b2e2736cc37887
json96ua <42866143+json96ua@users.noreply.github.com>
2019-01-24 18:18:21 +0200

    Add new methods in UserController

--- src/main/java/com/nc/airport/backend/controller/UserController.java
+++ src/main/java/com/nc/airport/backend/controller/UserController.java
@@ -1,9 +1,11 @@
 package com.nc.airport.backend.controller;
 
+import com.nc.airport.backend.model.dto.PasswordDTO;
 import com.nc.airport.backend.model.dto.ResponseFilteringWrapper;
 import com.nc.airport.backend.model.dto.SortingFilteringWrapper;
 import com.nc.airport.backend.model.entities.model.users.User;
 import com.nc.airport.backend.service.UserService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 
 import java.math.BigInteger;
@@ -28,6 +30,26 @@ public class UserController {
     @PostMapping
     public User addNewUser(@RequestBody User entity) {
         return service.saveNewUser(entity);
+    }
+
+    @GetMapping("/login={login}")
+    public User getUserByLogin(@PathVariable String login) {
+        return service.findByLogin(login);
+    }
+
+    @PostMapping("changePassword/login={login}")
+    public boolean changeUserPassword(
+            @PathVariable String login, @RequestBody PasswordDTO passwordDTO
+            ) {
+        User user = service.findByLogin(login);
+        BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+        if (encoder.matches(passwordDTO.getOldPassword(), user.getPassword())) {
+            user.setPassword(encoder.encode(passwordDTO.getNewPassword()));
+            service.saveEntity(user);
+            return true;
+        } else {
+            return false;
+        }
     }
 
     @PutMapping
