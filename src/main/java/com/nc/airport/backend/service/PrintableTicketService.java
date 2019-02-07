8cb42f23662945a053020887a5b42d53fb113d62
Anton K <aksileich@gmail.com>
2019-02-07 03:30:37 +0200

    Method for getting a ticket by providing a flight id and passport serial number

--- src/main/java/com/nc/airport/backend/service/PrintableTicketService.java
+++ src/main/java/com/nc/airport/backend/service/PrintableTicketService.java
@@ -8,19 +8,30 @@ import com.nc.airport.backend.model.entities.model.airplane.SeatType;
 import com.nc.airport.backend.model.entities.model.flight.Airport;
 import com.nc.airport.backend.model.entities.model.flight.Flight;
 import com.nc.airport.backend.model.entities.model.ticketinfo.Passenger;
+import com.nc.airport.backend.model.entities.model.ticketinfo.Passport;
 import com.nc.airport.backend.model.entities.model.ticketinfo.Ticket;
+import com.nc.airport.backend.persistence.eav.annotations.attribute.value.ReferenceField;
+import com.nc.airport.backend.persistence.eav.annotations.attribute.value.ValueField;
+import com.nc.airport.backend.persistence.eav.entity2mutable.util.ReflectionHelper;
 import com.nc.airport.backend.persistence.eav.exceptions.DatabaseConsistencyException;
+import com.nc.airport.backend.persistence.eav.mutable2query.filtering2sorting.filtering.FilterEntity;
 import com.nc.airport.backend.persistence.eav.repository.EavCrudRepository;
 import com.nc.airport.backend.model.dto.PrintableTicket;
+import com.nc.airport.backend.persistence.eav.repository.Page;
 import com.nc.airport.backend.util.mail.TicketSender;
 import com.nc.airport.backend.util.print.pdf.PdfTicketGenerator;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.io.ByteArrayOutputStream;
 import java.math.BigInteger;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 import java.util.Optional;
 
+@Log4j2
 @Service
 public class PrintableTicketService {
     private EavCrudRepository repository;
@@ -44,6 +55,32 @@ public class PrintableTicketService {
         ticketPdf = outputStream.toByteArray();
 
         return TicketSender.sendTicketById(ticketPdf, recipientEmail);
+    }
+
+    public Ticket findTicketByFlightAndPassport(BigInteger flightId, String passportSerialNumber) {
+
+        Passport passport;
+        Passenger passenger;
+
+        List<FilterEntity> passportRequest = Collections.singletonList(requestThePassportBySerialNumber(passportSerialNumber));
+        List<BaseEntity> requestResults =
+                repository.findSlice(Passport.class, new Page(0), null, passportRequest);
+        if (requestResults.size() > 1) {
+            log.info("Database returned more than one passport with serial number " + passportSerialNumber +
+                    "It's possible that it is attempt to get passport by using only a part of serial number. Thus, it's invalid");
+            return null;
+        }
+        passport = (Passport) requestResults.get(0);
+
+        List<FilterEntity> passengerRequest = Collections.singletonList(requestThePassengerByPassportId(passport.getObjectId()));
+        passenger = (Passenger) repository.findSlice(Passenger.class, new Page(0), null, passengerRequest).get(0);
+
+        List<FilterEntity> ticketRequest = Arrays.asList(
+                requestTheTicketByFlightId(flightId),
+                requestTheTicketByPassengerId(passenger.getObjectId())
+            );
+        List<BaseEntity> ticketRequestResults = repository.findSlice(Ticket.class, new Page(0), null, ticketRequest);
+        return (Ticket) ticketRequestResults.get(0);
     }
 
     public PrintableTicket getPrintableTicket(Ticket ticket) {
@@ -92,7 +129,32 @@ public class PrintableTicketService {
                         + entityClass.getSimpleName()));
     }
 
-    private double calculateCost(Flight flight, Seat seat, SeatType seatType) {
-        return flight.getBaseCost().doubleValue() + seat.getModifier() + seatType.getModifier();
+//    private double calculateCost(Flight flight, Seat seat, SeatType seatType) {
+//        return flight.getBaseCost().doubleValue() + seat.getModifier() + seatType.getModifier();
+//    }
+
+    private FilterEntity requestThePassportBySerialNumber(String passportSerialNumber) {
+        BigInteger passportSerialNumAttributeId =
+                ReflectionHelper.getAttributeIdByFieldName(Passport.class, ValueField.class, "serialNumber");
+        return new FilterEntity(passportSerialNumAttributeId, Collections.singleton(passportSerialNumber));
     }
+
+    private FilterEntity requestThePassengerByPassportId(BigInteger passportId) {
+        BigInteger referenceId =
+                ReflectionHelper.getAttributeIdByFieldName(Passenger.class, ReferenceField.class, "passportId");
+        return new FilterEntity(referenceId, Collections.singleton(passportId));
+    }
+
+    private FilterEntity requestTheTicketByFlightId(BigInteger flightId) {
+        BigInteger flightIdReferenceId =
+                ReflectionHelper.getAttributeIdByFieldName(Ticket.class, ReferenceField.class, "flightId");
+        return new FilterEntity(flightIdReferenceId, Collections.singleton(flightId));
+    }
+
+    private FilterEntity requestTheTicketByPassengerId(BigInteger passengerId) {
+        BigInteger passengerIdReferenceId =
+                ReflectionHelper.getAttributeIdByFieldName(Ticket.class, ReferenceField.class, "passengerId");
+        return new FilterEntity(passengerIdReferenceId, Collections.singleton(passengerId));
+    }
+
 }
